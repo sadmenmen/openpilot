e0ea6d4ab6accadca6668f50c0f153b5120c2793
hellokitty-666 <84395321+hellokitty-666@users.noreply.github.com>
2021-05-20 11:20:36 +0800

    Add files via upload

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -1,11 +1,9 @@
 from cereal import car
 from selfdrive.car import apply_std_steer_torque_limits
 from selfdrive.car.volkswagen import volkswagencan
-from selfdrive.car.volkswagen.values import DBC, CANBUS, MQB_LDW_MESSAGES, BUTTON_STATES, CarControllerParams
+from selfdrive.car.volkswagen.values import DBC, CANBUS, NetworkLocation, MQB_LDW_MESSAGES, BUTTON_STATES, CarControllerParams
 from opendbc.can.packer import CANPacker
-from common.params import Params
-params = Params()
-Turn_Lamp_info = params.get_bool('Turn_Lamp')
+
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
@@ -19,9 +17,12 @@ class CarController():
     self.graMsgSentCount = 0
     self.graMsgStartFramePrev = 0
     self.graMsgBusCounterPrev = 0
-    # dp
-    self.last_blinker_on = False
-    self.blinker_end_frame = 0.
+
+    if CP.networkLocation == NetworkLocation.fwdCamera:
+      self.ext_can = CANBUS.pt
+    else:
+      self.ext_can = CANBUS.cam
+
     self.steer_rate_limited = False
 
   def update(self, enabled, CS, frame, actuators, visual_alert, left_lane_visible, right_lane_visible, left_lane_depart, right_lane_depart):
@@ -97,14 +98,7 @@ class CarController():
         # Continue sending HCA_01 messages, with the enable flags turned off.
         hcaEnabled = False
         apply_steer = 0
-      # dp
-      if Turn_Lamp_info:
-        blinker_on = CS.out.leftBlinker or CS.out.rightBlinker
-        if self.last_blinker_on and not blinker_on:
-          self.blinker_end_frame = frame
-        if blinker_on:
-          apply_steer = 0 if isinstance(apply_steer, int) else False
-        self.last_blinker_on = blinker_on
+
       self.apply_steer_last = apply_steer
       idx = (frame / P.HCA_STEP) % 16
       can_sends.append(volkswagencan.create_mqb_steering_control(self.packer_pt, CANBUS.pt, apply_steer,
@@ -189,7 +183,7 @@ class CarController():
         if self.graMsgSentCount == 0:
           self.graMsgStartFramePrev = frame
         idx = (CS.graMsgBusCounter + 1) % 16
-        can_sends.append(volkswagencan.create_mqb_acc_buttons_control(self.packer_pt, CANBUS.pt, self.graButtonStatesToSend, CS, idx))
+        can_sends.append(volkswagencan.create_mqb_acc_buttons_control(self.packer_pt, self.ext_can, self.graButtonStatesToSend, CS, idx))
         self.graMsgSentCount += 1
         if self.graMsgSentCount >= P.GRA_VBP_COUNT:
           self.graButtonStatesToSend = None
