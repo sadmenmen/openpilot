f4d9efae4dfa111756311b7140c252b47dc8c440
hellokitty-666 <84395321+hellokitty-666@users.noreply.github.com>
2021-05-21 23:52:35 +0800

    Update sidebar.cc

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -107,7 +107,7 @@ void Sidebar::paintEvent(QPaintEvent *event) {
   p.drawText(r, Qt::AlignCenter, network_type[net_type]);
 
   // metrics
-  drawMetric(p, "TEMP", QString("%1°C").arg(temp_val), temp_status, 338);
+  drawMetric(p, "温度", QString("%1°C").arg(temp_val), temp_status, 338);
   drawMetric(p, panda_str, "", panda_status, 518);
-  drawMetric(p, "CONNECT\n" + connect_str, "", connect_status, 676);
+  drawMetric(p, "网络\n" + connect_str, "", connect_status, 676);
 }
