c40a182f5846759b1febe67efe2cce928f56bbb1
“用menwenliang <809079697@qq.com>
2021-05-21 19:55:27 +0800

    恢复 bug

--- selfdrive/hardware/base.h
+++ selfdrive/hardware/base.h
@@ -16,7 +16,7 @@ public:
   static void set_brightness(int percent) {}
   static void set_display_power(bool on) {}
 
-  static bool get_ssh_enabled(bool enabled) {}
+  static bool get_ssh_enabled() {bool enabled; }
   static void set_ssh_enabled(bool enabled) {}
 
   static bool PC() { return false; }
