946dd2a86c7064762bb9c91f10f2f47983ece5fb
“用menwenliang <809079697@qq.com>
2021-06-01 14:31:48 +0800

    fix lowb

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -170,7 +170,7 @@ class Controls:
             self.startup_event = None
 
         # Create events for battery, temperature, disk space, and memory
-        if self.sm['deviceState'].batteryPercent < 1 and self.sm['deviceState'].chargingError:
+        if self.sm['deviceState'].batteryPercent < 0 and self.sm['deviceState'].chargingError:
             # at zero percent battery, while discharging, OP should not allowed
             self.events.add(EventName.lowBattery)
         if self.sm['deviceState'].thermalStatus >= ThermalStatus.red:
