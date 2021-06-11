dd9e83fcf21fdf792a7c3b56aea525c8d06b87d6
hellokitty-666 <84395321+hellokitty-666@users.noreply.github.com>
2021-06-11 09:16:02 +0800

    Update car_helpers.py

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -91,7 +91,7 @@ def fingerprint(logcan, sendcan):
     fixed_fingerprint = car_content
   else:
     fixed_fingerprint = os.environ.get('FINGERPRINT', "")
-  skip_fw_query = os.environ.get('SKIP_FW_QUERY', "1"
+  skip_fw_query = os.environ.get('SKIP_FW_QUERY', "1")
 
   if not fixed_fingerprint and not skip_fw_query:
     # Vin query only reliably works thorugh OBDII
