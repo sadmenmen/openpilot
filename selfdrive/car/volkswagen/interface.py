185512f42192a091b33064f435e1ae42df30c667
hellokitty-666 <84395321+hellokitty-666@users.noreply.github.com>
2021-06-14 20:56:50 +0800

    更新 interface.py

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -49,7 +49,7 @@ class CarInterface(CarInterfaceBase):
       if 0xfd in fingerprint[1]:  # ESP_21 present on bus 1, we're hooked up at the CAN gateway
         ret.networkLocation = NetworkLocation.gateway
       else:  # We're hooked up at the LKAS camera
-        ret.networkLocation = NetworkLocation.gateway
+        ret.networkLocation = NetworkLocation.fwdCamera
       cloudlog.info("Detected network location: %s", ret.networkLocation)
 
     # Global tuning defaults, can be overridden per-vehicle
