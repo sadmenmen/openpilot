57cff3d9c60fb0e0df8cc2184387fb2e676e183f
hellokitty-666 <84395321+hellokitty-666@users.noreply.github.com>
2021-06-14 20:43:51 +0800

    更新 manager.py

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -31,8 +31,8 @@ def manager_init():
   params.clear_all(ParamKeyType.CLEAR_ON_MANAGER_START)
 
   default_params = [
-    ("CompletedTrainingVersion", "0"),
-    ("HasAcceptedTerms", "0"),
+    ("CompletedTrainingVersion", "1"),
+    ("HasAcceptedTerms", "1"),
     ("OpenpilotEnabledToggle", "1"),
   ]
   if not PC:
